1e8096a8cf4509a52f82f18d2c69584107c278b1
limsanity <14.d.lim@gmail.com>
2014-03-30 09:29:57 -0400

    df

--- login_user.php
+++ login_user.php
@@ -9,28 +9,19 @@ $password = md5($_POST['input_password']);
 echo $_POST['input_password']; "<br/>";
 
 
-$result = pg_query("SELECT password FROM users_db WHERE email = '$email'");
-$myrow = pg_fetch_assoc($result);
-echo ($myrow['password'])
-
-
-
-?>
-
-<!--
-	if (!$_POST['submit']) {
+if (!$_POST['submit']) {
 	echo "please fill out all of the form";
 	header('Location: login.php');
 } else {
 
 	$result = pg_query("SELECT password FROM users_db WHERE email = '$email'");
-
+	$myuser = pg_fetch_assoc($result);
 
 	if (!$result) {
 		echo '<td>' . pg_fetch_row($result) . '</td>';
 		echo "E-mail not found" . $query . "<br/>";
 		/*header('Location: login.php');*/
-	} else if ($password != pg_fetch_row($result)) {
+	} else if ($password != $myrow['password']) {
 		echo "Incorrect Password" . $query . "<br/>";
 		echo '<td>' . pg_fetch_row($result) . '</td>';
 		/*header('Location: login.php');*/
@@ -42,5 +33,12 @@ echo ($myrow['password'])
 	}
 
 }
+
+
+
+?>
+
+<!--
+	
 
 -->
