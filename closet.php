296ca6b819ba62e89d03c565f9e18d4c5b81e5c8
limsanity <14.d.lim@gmail.com>
2014-03-30 13:04:59 -0400

    last minute

--- closet.php
+++ closet.php
@@ -22,9 +22,9 @@ while ($row = pg_fetch_assoc($result)) {
 	$result = '';
 	$result .= '<a class="item" href="listing.html">';
 	$result .= '<div class="thumb_container">';
-	$result .= '<img class="thumb" src="bundle_images/' . $row['image_id'] . '" alt="image could not be loaded :(">';
+	$result .= '<img class="thumb" src="./bundle_images/' . $row['image_id'] . '" alt="image could not be loaded :(">';
 	$result .= "<div>";
-	$result .= '<p class="item_description">[' . $row['gender'] . '][' . $row[age_rang] . ']</p>';
+	$result .= '<p class="item_description">[' . $row['gender'] . '][' . $row['age_rang'] . ']</p>';
 	$result .= "</a>";
 	$result .= " ";
 	
@@ -34,7 +34,43 @@ while ($row = pg_fetch_assoc($result)) {
 echo '</div> ';
 
 
+$query = "SELECT * FROM bundles";
 
+$result = pg_query($query);
+if (!$result) {
+	echo "Problem with query " . $query . "<br/>";
+	echo pg_last_error();
+	exit();
+}
+
+
+$i = 0;
+echo '<html><br /><br /><br /><br /><br /><br /><body><table><tr>';
+while ($i < pg_num_fields($result))
+{
+	$fieldName = pg_field_name($result, $i);
+	echo '<td>' . $fieldName . '</td>';
+	$i = $i + 1;
+}
+echo '</tr>';
+$i = 0;
+
+while ($row = pg_fetch_row($result)) 
+{
+	echo '<tr>';
+	$count = count($row);
+	$y = 0;
+	while ($y < $count)
+	{
+		$c_row = current($row);
+		echo '<td>' . $c_row . '</td>';
+		next($row);
+		$y = $y + 1;
+	}
+	echo '</tr>';
+	$i = $i + 1;
+}
+pg_free_result($result);
 
 ?>
 
