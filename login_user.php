7b233997c7946fb8f0e63d88d92a261e0797bc57
limsanity <14.d.lim@gmail.com>
2014-03-30 02:28:39 -0400

    df

--- login_user.php
+++ login_user.php
@@ -6,27 +6,23 @@ $email = $_POST['input_email'];
 /* $user_name = $_POST['input_name_first']; */
 $password = md5($_POST['input_password']);
 
-echo '<td>' . "HELLLLOOO" . '</td>';
-
 /**** CHECK FOR PASSWORD LENGTH ***/
 if (!$_POST['submit']) {
 	echo "please fill out all of the form";
 	header('Location: create_user.php');
 } else {
-	$query = "SELECT password FROM users_db WHERE email == '$email'";
-	$result = pg_query($query);
-	
+
+	$result = pg_query("SELECT password FROM users_db WHERE email == '$email'");
+
 	if (!$result) {
 		echo "E-mail not found" . $query . "<br/>";
 		header('Location: login.php');
-	}
-
-	else if ($password != $result) {
+	} else if ($password != $result) {
 		echo "E-mail not found" . $query . "<br/>";
 		header('Location: login.php');
 	} else {
 		/* go to listings page */
-		 header('Location: index.php'); 
+		header('Location: index.php');
 	}
 
 }
