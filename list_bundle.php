f42c7f1c9902ee83c2847f5cf5c45400a0ed5250
limsanity <14.d.lim@gmail.com>
2014-03-30 13:28:16 -0400

    last minute

--- list_bundle.php
+++ list_bundle.php
@@ -46,15 +46,15 @@ if ((($_FILES["file"]["type"] == "image/gif")
     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-    if (file_exists("bundle_images/" . $_FILES["file"]["name"]))
+    if (file_exists("./bundle_images/" . $_FILES["file"]["name"]))
       {
       echo $_FILES["file"]["name"] . " already exists. ";
       }
     else
       {
       move_uploaded_file($_FILES["file"]["tmp_name"],
-      "bundle_images/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "bundle_images/" . $_FILES["file"]["name"];
+      "./bundle_images/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "./bundle_images/" . $_FILES["file"]["name"];
       }
     }
   }
