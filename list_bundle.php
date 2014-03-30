7d5776ee527271ffde50a8467033ffdc3a94338c
limsanity <14.d.lim@gmail.com>
2014-03-30 12:32:48 -0400

    last minute

--- list_bundle.php
+++ list_bundle.php
@@ -17,18 +17,18 @@ if (!$_POST['submit']) {
 if ($selected_radio == 'male') {
 
 	pg_query("INSERT INTO bundles (gender))
-					   VALUES('$male',)") or die('Error: ' . pg_last_error());
+					   VALUES('$male')") or die('Error: ' . pg_last_error());
 } else if ($selected_radio == 'female') {
 
 	pg_query("INSERT INTO bundles (gender))
-					   VALUES('$female',)") or die('Error: ' . pg_last_error());
+					   VALUES('$female')") or die('Error: ' . pg_last_error());
 }
 
 pg_query("INSERT INTO bundles (age_range))
-					   VALUES('$age',)") or die('Error: ' . pg_last_error());
+					   VALUES('$age')") or die('Error: ' . pg_last_error());
 
 pg_query("INSERT INTO bundles (image_id))
-					   VALUES('$file_name',)") or die('Error: ' . pg_last_error());
+					   VALUES('$file_name')") or die('Error: ' . pg_last_error());
 
 file_put_contents('./bundle_images', $image);
 
