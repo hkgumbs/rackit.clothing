12bd0e1f33a9abb647ebaf91b03d3d98484795fb
limsanity <14.d.lim@gmail.com>
2014-03-30 12:45:56 -0400

    last minute

--- list_bundle.php
+++ list_bundle.php
@@ -27,7 +27,7 @@ if ($selected_radio == 'male') {
 pg_query("INSERT INTO bundles (age_rang)
 					   VALUES('$age')") or die('Error: ' . pg_last_error());
 
-pg_query("INSERT INTO bundles (image_id))
+pg_query("INSERT INTO bundles (image_id)
 					   VALUES('$file_name')") or die('Error: ' . pg_last_error());
 
 
