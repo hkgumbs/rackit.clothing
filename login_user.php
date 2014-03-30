3e0b4fc279078dfd4521cfda3dfe22d5718b8efc
limsanity <14.d.lim@gmail.com>
2014-03-30 03:05:33 -0400

    df

--- login_user.php
+++ login_user.php
@@ -19,13 +19,13 @@ if (!$_POST['submit']) {
 	
 	if (!$result) {
 		echo "E-mail not found" . $query . "<br/>";
-		/*header('Location: login.php');
+		header('Location: login.php');
 	} else if ($password != $result) {
 		echo "Incorrect Password" . $query . "<br/>";
-		header('Location: login.php');*/
+		header('Location: login.php');
 	} else {
 		/* go to listings page */
-		/*header('Location: index.php');*/
+		header('Location: index.php');
 	}
 
 }
