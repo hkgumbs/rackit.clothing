49acdc69b85709d21c3e2b91c7feb87df1c6c376
limsanity <14.d.lim@gmail.com>
2014-03-30 02:26:49 -0400

    df

--- login_user.php
+++ login_user.php
@@ -13,21 +13,20 @@ if (!$_POST['submit']) {
 	echo "please fill out all of the form";
 	header('Location: create_user.php');
 } else {
-
 	$query = "SELECT password FROM users_db WHERE email == '$email'";
-
 	$result = pg_query($query);
+	
 	if (!$result) {
 		echo "E-mail not found" . $query . "<br/>";
 		header('Location: login.php');
 	}
 
-	if ($password != $result) {
+	else if ($password != $result) {
 		echo "E-mail not found" . $query . "<br/>";
 		header('Location: login.php');
 	} else {
-		/* go to listings page
-		header('Location: listings.php'); */
+		/* go to listings page */
+		 header('Location: index.php'); 
 	}
 
 }
