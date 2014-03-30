24f9c503c6cac3fbee1d3916c74e4c2ba6b3842a
limsanity <14.d.lim@gmail.com>
2014-03-30 13:22:48 -0400

    last minute

--- list_bundle.php
+++ list_bundle.php
@@ -5,7 +5,6 @@ include "includes/connection.php";
 
 $selected_radio = $_POST['gender'];
 $age = $_POST['input_age'];
-$file_name = md5($_POST['file']);
 
 if (!$_POST['submit']) {
 	echo "please fill out all of the form";
@@ -23,22 +22,52 @@ $gender = "F";
 }
 
 
-pg_query("INSERT INTO bundles (gender,age_rang,image_id)
-					   VALUES('$gender','$age','$file_name')") or die('Error: ' . pg_last_error());
 
-
-if ($_FILES["file"]["error"] > 0)
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 20000)
+&& in_array($extension, $allowedExts))
   {
-  echo "Error: " . $_FILES["file"]["error"] . "<br>";
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+    if (file_exists("bundle_images/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "bundle_images/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "bundle_images/" . $_FILES["file"]["name"];
+      }
+    }
   }
 else
   {
-  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-  echo "Type: " . $_FILES["file"]["type"] . "<br>";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-  echo "Stored in: " . $_FILES["$file_name"]["./bundle_images"];
+  echo "Invalid file";
   }
   
-  
 header('Location: closet.php');
+
+
+
+pg_query("INSERT INTO bundles (gender,age_rang,image_id)
+					   VALUES('$gender','$age','$temp')") or die('Error: ' . pg_last_error());
+
 ?>
