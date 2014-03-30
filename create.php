eea8b3b21cccd47ef0310dd2b0d32388062d6c08
limsanity <14.d.lim@gmail.com>
2014-03-29 23:16:47 -0400

    fixed login button

--- create.php
+++ create.php
@@ -6,7 +6,7 @@
 	 /* $user_name = $_POST['input_name_first']; */
 	$password = md5($_POST['input_password']);
 
-	if(!$_POST['submit']) {
+	if(!$_POST['button_submit']) {
 		echo "please fill out the form";
 		header('Location: index.php');
 		
@@ -15,7 +15,7 @@
 		pg_query("INSERT INTO users_db (email, password)
 					   VALUES('$email','$password')") or die('Error: ' . pg_last_error());
 		echo "User has been added!";
-		header('Location: index.php');
+		header('Location: login.php');
 	}
 
 ?>
