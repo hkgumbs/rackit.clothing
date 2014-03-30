207399b55049e72ff50f2d04564b8dda92a7815a
limsanity <14.d.lim@gmail.com>
2014-03-29 23:12:55 -0400

    fixed login button

--- index.php
+++ index.php
@@ -1,46 +1,6 @@
 <?php
 include_once ("home.html");
 
-include "includes/connection.php";
-
-$query = "SELECT * FROM users_db";
-
-$result = pg_query($query);
-if (!$result) {
-	echo "Problem with query " . $query . "<br/>";
-	echo pg_last_error();
-	exit();
-}
-
-
-$i = 0;
-echo '<html><br /><br /><br /><br /><br /><br /><body><table><tr>';
-while ($i < pg_num_fields($result))
-{
-	$fieldName = pg_field_name($result, $i);
-	echo '<td>' . $fieldName . '</td>';
-	$i = $i + 1;
-}
-echo '</tr>';
-$i = 0;
-
-while ($row = pg_fetch_row($result)) 
-{
-	echo '<tr>';
-	$count = count($row);
-	$y = 0;
-	while ($y < $count)
-	{
-		$c_row = current($row);
-		echo '<td>' . $c_row . '</td>';
-		next($row);
-		$y = $y + 1;
-	}
-	echo '</tr>';
-	$i = $i + 1;
-}
-pg_free_result($result);
-
 ?>
 
 <!--
