869371794342eff49e0dcfab24510ab7c27c025f
limsanity <14.d.lim@gmail.com>
2014-03-30 01:52:37 -0400

    adding login

--- login_user.php
+++ login_user.php
@@ -7,17 +7,14 @@ $email = $_POST['input_email'];
 /* $user_name = $_POST['input_name_first']; */
 $password = md5($_POST['input_password']);
 
+echo '<td>' . "HELLLLOOO". '</td>';
+
 /**** CHECK FOR PASSWORD LENGTH ***/
 if (!$_POST['submit']) {
 	echo "please fill out all of the form";
 	header('Location: create_user.php');
-} else if ($password != $password_confirm) {
-	echo "passwords don't match";
-	header('Location: create_user.php');
 } else {
-	pg_query("INSERT INTO users_db (email, password, address_street, address_city, address_state, address_zip_code)
-					   VALUES('$email','$password', '$street_address', '$city', '$state', '$zip')") or die('Error: ' . pg_last_error());
-	echo "User has been added!";
+	pg_query("SELECT count(1) FROM users_db WHERE password = '$password'");
 	header('Location: login.php');
 }
 ?>
