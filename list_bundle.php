525fca0dc4d695a7fc442e26a0c437d647a4c6c5
limsanity <14.d.lim@gmail.com>
2014-03-30 13:32:15 -0400

    last minute

--- list_bundle.php
+++ list_bundle.php
@@ -13,61 +13,39 @@ if (!$_POST['submit']) {
 
 if ($selected_radio == 'male') {
 
-$gender = "M";
-	
+	$gender = "M";
+
 } else if ($selected_radio == 'female') {
 
-$gender = "F";
+	$gender = "F";
 
 }
-
-
 
 $allowedExts = array("gif", "jpeg", "jpg", "png");
 $temp = explode(".", $_FILES["file"]["name"]);
 $extension = end($temp);
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/jpg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/x-png")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& ($_FILES["file"]["size"] < 20000)
-&& in_array($extension, $allowedExts))
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-    }
-  else
-    {
-    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-    echo "Type: " . $_FILES["file"]["type"] . "<br>";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/pjpeg") || ($_FILES["file"]["type"] == "image/x-png") || ($_FILES["file"]["type"] == "image/png")) && ($_FILES["file"]["size"] < 20000) && in_array($extension, $allowedExts)) {
+	if ($_FILES["file"]["error"] > 0) {
+		echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+	} else {
+		echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+		echo "Type: " . $_FILES["file"]["type"] . "<br>";
+		echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+		echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-    if (file_exists("./bundle_images/" . $_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists. ";
-      }
-    else
-      {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "./bundle_images/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "./bundle_images/" . $_FILES["file"]["name"];
-      }
-    }
-  }
-else
-  {
-  echo "Invalid file";
-  }
-  
-header('Location: closet.php');
-
-
+		if (file_exists("./bundle_images/" . $_FILES["file"]["name"])) {
+			echo $_FILES["file"]["name"] . " already exists. ";
+		} else {
+			move_uploaded_file($_FILES["file"]["tmp_name"], "./bundle_images/" . $_FILES["file"]["name"]);
+			echo "Stored in: " . "./bundle_images/" . $_FILES["file"]["name"];
+		}
+	}
+} else {
+	echo "Invalid file";
+}
 
 pg_query("INSERT INTO bundles (gender,age_rang,image_id)
 					   VALUES('$gender','$age','$temp')") or die('Error: ' . pg_last_error());
 
+header('Location: closet.php');
 ?>
