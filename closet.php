d7437efd21c9062ff1ce1cfe377d2e067c7f4997
limsanity <14.d.lim@gmail.com>
2014-03-30 10:48:12 -0400

    trying closet with search

--- closet.php
+++ closet.php
@@ -27,7 +27,14 @@ while ($row = mysql_fetch_array($result)) {
 	$result .= " ";
 }
 
-        
+$result .= '<a class="item" href="listing.html">';
+$result .= '<div class="thumb_container">';
+$result .= '<img class="thumb" src="style/photo_default.png" alt="image could not be loaded :(">';
+$result .= '</div>';
+$result .= '<p class="item_description">[gender][age]</p>';
+$result .= '</a>';
+$result .= " ";
+
 ?>
 
 <div id="mydiv"><?php echo $result; ?></div> 
