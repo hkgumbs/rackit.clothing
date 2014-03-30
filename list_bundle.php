2fe3f95e518e5c4bf3c4184687c476907eeb89c0
limsanity <14.d.lim@gmail.com>
2014-03-30 12:15:49 -0400

    last minute

--- list_bundle.php
+++ list_bundle.php
@@ -3,9 +3,32 @@ include_once ("upload.html");
 
 include "includes/connection.php";
 
-$file_name = md5($_POST['input_new_email']);
+$selected_radio = $_POST['gender'];
+$age = $_POST['input_age'];
+$file_name = md5($_POST['file']);
+$male = 'M';
+$female = 'F';
 
+if (!$_POST['submit']) {
+	echo "please fill out all of the form";
+	header('Location: signup.php');
+}
 
+if ($selected_radio == 'male') {
 
+	pg_query("INSERT INTO bundles (gender))
+					   VALUES('$male',)") or die('Error: ' . pg_last_error());
+} else if ($selected_radio == 'female') {
 
+	pg_query("INSERT INTO bundles (gender))
+					   VALUES('$female',)") or die('Error: ' . pg_last_error());
+}
+
+pg_query("INSERT INTO bundles (age_range))
+					   VALUES('$age',)") or die('Error: ' . pg_last_error());
+
+pg_query("INSERT INTO bundles (image_id))
+					   VALUES('$file_name',)") or die('Error: ' . pg_last_error());
+					
+					file_put_contents('./myDir/myFile.gif', $image);
 ?>
