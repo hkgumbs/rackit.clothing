d80c173df2bd762e4177371d5b77b96fcc6a7db8
limsanity <14.d.lim@gmail.com>
2014-04-05 18:41:14 -0400

    CHANGED LOTS OF VARIABLE NAMES

--- index.php
+++ index.php
@@ -3,26 +3,4 @@ include_once ("home.html");
 
 ?>
 
-<!--
-<h1>Create Account</h1>
-<form action = "create.php" method = "post">
-	E-mail
-	<input type="text" name="input_email" value="" />
-	<br />
-	Password
-	<input type="text" name="password" value="" />
-	<br />
-	<br />
-	<input type="submit" name="submit">
-</form>
-
-<h1>Delete Account</h1>
-<form action = "delete.php" method = "post">
-	E-mail
-	<input type="text" name="input_email" value="" />
-	<br />
-	<br />
-	<input type="submit" name="submit">
-</form>
--->
 
