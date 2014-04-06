441624a670580f1329c3785f9995a34fc39c0b2b
limsanity <14.d.lim@gmail.com>
2014-04-05 21:09:45 -0400

    better table names

--- login_user.php
+++ login_user.php
@@ -13,7 +13,7 @@ if (!$_POST['submit']) {
 	header('Location: login.php');
 } else {
 	
-	$result = pg_query("SELECT password FROM User WHERE email = '$email'");
+	$result = pg_query("SELECT password FROM user WHERE email = '$email'");
 	$myuser = pg_fetch_assoc($result);
 
 	if (!$result) {
