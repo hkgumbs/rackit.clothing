633d93aa463e3d53a24474ff73c46f87848afa6c
limsanity <14.d.lim@gmail.com>
2014-04-07 10:48:45 -0400

    password length

--- create_user.php
+++ create_user.php
@@ -5,9 +5,11 @@ include "include/sendgrid-php/sendgrid-php.php";
 
 /* Make all emails lowercase so they compare alright */
 /* eg DAVID@gmail.com should = david@gmail.com */
-$email = strtolower($_POST['input_new_email']);
+$email = strtolower(htmlspecialchars($_POST['input_new_email']));
 /* $person_name = $_POST['input_name_first']; */
-$password = trim(md5($_POST['input_password']));
+$password = trim(htmlspecialchars($_POST['input_password']));
+$pLength = strlen($password);
+$password = md5($password);
 $password_confirm = trim(md5($_POST['input_password_confirm']));
 $street_address = $_POST['input_street_address'];
 $city = $_POST['input_city'];
@@ -15,16 +17,23 @@ $state = $_POST['input_state'];
 $zip = $_POST['input_zip'];
 
 /*
-$sendgrid = new SendGrid('davidlim14', 'doublehappiness14');
-$email    = new SendGrid\Email();
-$email->addTo($email)->
-       setFrom('rackit@gmail.com')->
-       setSubject('Account Confirmation!')->
-       setHtml('<strong>Welcome! Please confirm your email by clicking the link below</strong><p><a href="http://rackit.clothing/verify.php?email=' . urlencode($email) . '&v=' . md5("yay" . $email) . '"></a></p>');
+ $sendgrid = new SendGrid('davidlim14', 'doublehappiness14');
+ $email    = new SendGrid\Email();
+ $email->addTo($email)->
+ setFrom('rackit@gmail.com')->
+ setSubject('Account Confirmation!')->
+ setHtml('<strong>Welcome! Please confirm your email by clicking the link below</strong><p><a href="http://rackit.clothing/verify.php?email=' . urlencode($email) . '&v=' . md5("yay" . $email) . '"></a></p>');
 
-$sendgrid->send($email);*/
+ $sendgrid->send($email);*/
 
-/**** CHECK FOR PASSWORD LENGTH ***/
+
+
+if ($pLength >= 6 && $pLength <= 16) {
+	$errorMessage = "";
+} else {
+	$errorMessage = $errorMessage . "Password must be between 6 and 16 characters" . "<BR>";
+}
+
 if (!$_POST['submit']) {
 	echo "please fill out all of the form";
 	header('Location: signup.php');
@@ -33,9 +42,12 @@ if (!$_POST['submit']) {
 	header('Location: signup.php');
 } else {
 	pg_query("INSERT INTO person (email, password, address_street, address_city, address_state, address_zipcode)
-					VALUES ('$email', '$password', '$street_address', '$city', '$state', '$zip')") or die ('Error: ' . pg_last_error());
-					
+					VALUES ('$email', '$password', '$street_address', '$city', '$state', '$zip')") or die('Error: ' . pg_last_error());
+
 	echo "person has been added!";
+
+	session_start();
+	$_SESSION['login'] = "1";
 	header('Location: help.html');
 }
 ?>
