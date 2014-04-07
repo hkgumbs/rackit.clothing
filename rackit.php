c2d4896c0efd2117c467d77c903491e5e61ba7e5
limsanity <14.d.lim@gmail.com>
2014-04-07 17:45:49 -0400

    trying out racking

--- rackit.php
+++ rackit.php
@@ -14,5 +14,7 @@ pg_query("INSERT INTO person_bundle (bundle_id, racker_id)
 					VALUES ('$bundle_id', '$person_id')") or die('Error: ' . pg_last_error());
 
 
+/** Should change to rackings page **/
+header('Location: closet.php');
 
 ?>
