b5a7fa99cff8e3477be678843691488f9f6875cc
limsanity <14.d.lim@gmail.com>
2014-04-07 10:24:12 -0400

    trying out security

--- closet.php
+++ closet.php
@@ -1,4 +1,10 @@
 <?php
+
+session_start();
+if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
+	header ("Location: login.php");
+}
+
 include_once ("closet.html");
 
 include "includes/connection.php";
