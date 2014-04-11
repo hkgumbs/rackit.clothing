aa73ad93f9aad0a559d29216a66f9158f3dc9d12
limsanity <14.d.lim@gmail.com>
2014-04-11 12:51:40 -0400

    Changed Profile

--- listing.php
+++ listing.php
@@ -14,8 +14,8 @@ $person_id = $_SESSION['user_id'];
 
 $bundle_id = $_GET['id'];
 
+/********************************* Getting Num Racked **************************/
 $racked_count = 0;
-
 $num_result = pg_query("SELECT * FROM person_bundle WHERE bundle_id = '$bundle_id'");
 if (!$num_result) {
 	echo "Problem with query " . $query . "<br/>";
@@ -72,8 +72,7 @@ echo '<div class="full_container">';
 echo '<img class="full" src="./bundle_images/' . $mybundle['image_id'] . '" alt="image could not be loaded :(">';
 echo '</div>';
 echo '<div class="distance_time">';
-echo '<h2 class="distance">2.4 miles away</h2>';
-echo '<h3 class="time">6 minutes</h3>';
+
 echo '</div>';
 echo '<div class="button_container">';
 echo ' <a class="button" id="rackit" href="rackit.php?id='.$bundle_id.'" id="'.$bundle_id.'">RackIt</a>';
